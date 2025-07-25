46c4bbaebec0c3fb9f24256fc84842977bda2912
akiraraihaan <raihanakirar@gmail.com>
2025-07-25 18:27:23 +0700

    refactor: remove unused Footer import from layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import Footer from "../components/Footer";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
